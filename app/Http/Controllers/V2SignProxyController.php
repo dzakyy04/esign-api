37d83b3a88922eb6505e1f1d241ae851bbc09f57
Dewa Sheva Dzaky <dzakylinggau@gmail.com>
2026-03-10 16:10:58 +0700

    feat: add v2 sign api test

---
+++ app/Http/Controllers/V2SignProxyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class V2SignProxyController extends Controller
+{
+    public function signPdfEmail(Request $request)
+    {
+        $baseUrl  = env('PROXY_BASE_URL');
+        $username = env('PROXY_USERNAME');
+        $password = env('PROXY_PASSWORD');
+
+        $payload = [
+            'email'               => $request->input('email'),
+            'passphrase'          => $request->input('passphrase'),
+            'signatureProperties' => $request->input('signatureProperties'),
+            'file'                => $request->input('file'),
+        ];
+
+        $response = Http::withBasicAuth($username, $password)
+            ->post("{$baseUrl}/api/v2/sign/pdf", $payload);
+
+        return response($response->body(), $response->status())
+            ->withHeaders([
+                'Content-Type' => $response->header('Content-Type') ?? 'application/json',
+            ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserProxyController;
+use App\Http\Controllers\V2SignProxyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 // Check status user
-Route::get('/user/status/{nik}', [UserProxyController::class, 'getUserStatus']);
+Route::get('/user/status/{nik}', [UserProxyController::class, 'checkStatusUser']);
 // Get profile
 Route::get('/user/profile/{nik}', [UserProxyController::class, 'getProfile']);
 // Get expired certificate
 Route::get('/entity/cert/expired', [UserProxyController::class, 'getExpiredCertificate']);
 // Get certificate chain
 Route::get('/user/certificate/chain/{id}', [UserProxyController::class, 'getCertificateChain']);
+
+// Sign PDF (Email + Passphrase)
+Route::post('/v2/sign/pdf', [V2SignProxyController::class, 'signPdfEmail']);
